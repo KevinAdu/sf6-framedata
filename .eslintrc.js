42cd06b0768bd842a1209b7422dfe4d3088a4dd1
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2023-04-26 20:22:59 +0900

    Added eslint and prettier - Closes #2

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:react/jsx-runtime',
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'project': './tsconfig.json'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'prettier'
+    ],
+    'rules': {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 0,
+        'react/jsx-uses-react': 0,
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
